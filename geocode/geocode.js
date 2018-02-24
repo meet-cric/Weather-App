95721d7c8cf559371f26c2a7aeed5686c3ed160a
meet_cric <mp32445@gmai.com>
2018-02-24 17:57:57 +0530

    [TASK:adding promise]

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,24 +1,40 @@
-const request = require('request');
+const yargs = require('yargs');
+const axios = require('axios');
 
-var geocodeAddress = (address, callback) => {
-  var encodedAddress = encodeURIComponent(address);
-
-  request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-  }, (error, response, body) => {
-    if (error) {
-      callback('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-      callback('Unable to find that address.');
-    } else if (body.status === 'OK') {
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        latitude: body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng
-      });
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
     }
-  });
-};
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-module.exports.geocodeAddress = geocodeAddress;
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find that address.');
+  }
+
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherUrl = `https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/${lat},${lng}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API servers.');
+  } else {
+    console.log(e.message);
+  }
+});
